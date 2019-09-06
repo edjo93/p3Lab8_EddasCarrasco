6d98eb6f95b973b0382e5feb8b69d40a830a496d
edjo93 <eddas.carrasco@unitec.edu>
2019-09-06 17:15:53 -0600

    add

--- project/main.cpp
+++ project/main.cpp
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 #include<string>
+
 using namespace std;
 
 void listar();
 void modificar();
-void agregarAlumno();
+void addDepartamento();
 void eliminar();
-
+void listarDepartamento();
+void listarEmpleados();
+void jefeEmpleados(string);
+void depto(string);
 sqlite3 *conn;
 sqlite3_stmt *res;
 int error=0;
@@ -18,49 +22,145 @@ const char *tail;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	//agregarAlumno();
-	//modificar();
-	//listar();
-	eliminar();
-	system("pause");
+	char opcion;
+	char opcion2;
+	char opcion3;
+	string dpto;
+	string empno;
+	do{
+		cout<<"\n1-listar\n2-agregar\n3-eliminar\n4-listar jefe y empleado\n5-actualizar salario de empleados\n6-empleados de departamento\ns-salir\n?:";
+		cin>>opcion;
+		switch(opcion){
+			case'1':
+				do{
+					cout<<"\n1-deptnos\n2-empleados\n3-regresar\n?:";
+					cin>>opcion3;
+					switch(opcion3){
+						case'1':
+							//listar departamento
+							cout<<"\n---listar departamento--"<<endl;
+							listarDepartamento();
+							break;
+						case'2':
+							//listar empleados
+							cout<<"\n---listar empleados--"<<endl;
+							listarEmpleados();
+							break;
+						case'3':
+							break;
+						default:
+							cout<<"\nno se ingreso una opcion correcta\n";	
+								
+					}
+				}while(opcion3!='3');
+				break;
+			case'2':
+				do{
+					cout<<"\n1-deptno\n2-empleado\n3-regresar\n?:";
+					cin>>opcion2;
+					switch(opcion2){
+						case'1':
+							//add departamento
+							cout<<"\n---add departamento--"<<endl;
+							addDepartamento();
+							break;
+						case'2':
+							//add empleados
+							break;
+						case'3':
+							break;
+						default:
+							cout<<"\nno se ingreso una opcion correcta\n";	
+								
+					}
+				}while(opcion2!='3');
+				
+				break;
+			case'3':
+				eliminar();
+				break;
+			case'4':
+				cout<<"\nempno: ";
+				cin>>empno;
+				jefeEmpleados(empno);
+				break;
+			case'5':
+				break;			
+			case'6':
+				cout<<"ndpto: ";
+				cin>>dpto;
+				depto(dpto);
+				break;	
+			case's':
+				cout<<"\nhasta luego\n";
+				break;	
+			default:
+				cout<<"\nno se ingreso una opcion correcta\n";
+		}
+		
+	}while(opcion!='s');
 	return 0;
 }
 
 
-void listar(){
-	error=sqlite3_open("base1.db",&conn);
+void listarDepartamento(){
+	error=sqlite3_open("oracle-sample.db",&conn);
 	error=sqlite3_prepare_v2(conn,
-	        "select * from alumnos",1000,&res,&tail);
+	        "select * from dept",1000,&res,&tail);
+	if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}	        
 	while(sqlite3_step(res) == SQLITE_ROW){
-		cout<<sqlite3_column_text(res,0)<<endl;
-		cout<<sqlite3_column_text(res,1)<<endl;
-		cout<<sqlite3_column_text(res,2)<<endl;
-		cout<<"------"<<endl;
+		cout<<"deptno: "<<sqlite3_column_text(res,0)<<endl;
+		cout<<"dname: "<<sqlite3_column_text(res,1)<<endl;
+		cout<<"loc: "<<sqlite3_column_text(res,2)<<endl;
+		cout<<endl<<endl<<endl;
 	}	
 	sqlite3_close(conn);	
 }
 
-
-void agregarAlumno(){
+void listarEmpleados(){
+	error=sqlite3_open("oracle-sample.db",&conn);
+	error=sqlite3_prepare_v2(conn,
+	        "select * from emp",1000,&res,&tail);
+    if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	while(sqlite3_step(res) == SQLITE_ROW){
+		cout<<"emptno: "<<sqlite3_column_text(res,0)<<endl;
+		cout<<"ename: "<<sqlite3_column_text(res,1)<<endl;
+		cout<<"job: "<<sqlite3_column_text(res,2)<<endl;
+		cout<<"mgr: "<<sqlite3_column_text(res,3)<<endl;
+		cout<<"hiredate: "<<sqlite3_column_text(res,4)<<endl;
+		cout<<"sal: "<<sqlite3_column_text(res,5)<<endl;
+		cout<<"comm: "<<sqlite3_column_text(res,6)<<endl;
+		cout<<"deptno: "<<sqlite3_column_text(res,7)<<endl;
+		cout<<endl<<endl<<endl;
+	}	
+	sqlite3_close(conn);	
+}
+void addDepartamento(){
 	string query;
-	string nombre;
-	string carrera;
-	string cuanta;
-	error=sqlite3_open("base1.db",&conn);
+	string deptno;
+	string dname;
+	string loc;
+	error=sqlite3_open("oracle-sample.db",&conn);
+	cin.ignore(100,'\n');
+	cout<<"\ndeptno: ";
+	getline(cin,deptno);
 	
-	cout<<"\nadd alumno / query: ";
+	cout<<"\nnombre del departamento: ";
+	getline(cin,dname);
 	
-	cout<<"\ncuenta: ";
-	getline(cin,cuanta);
+	cout<<"\nlocation: ";
+	getline(cin,loc);
 	
-	cout<<"\nnombre del alumno: ";
-	getline(cin,nombre);
-	
-	cout<<"\ncarrera: ";
-	getline(cin,carrera);
-	
-	query="insert into alumnos values('"+cuanta+"','"+nombre+"','"+carrera+"')";
+	query="insert into dept values('"+deptno+"','"+dname+"','"+loc+"')";
 	error=sqlite3_exec(conn,query.c_str(),0,0,0);
+	if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	
 	sqlite3_close(conn);
 }
 		
@@ -74,8 +174,96 @@ void modificar(){
 }		
 
 		
-void eliminar()	{//elimina datos null
-	error=sqlite3_open("base1.db",&conn);
-	error=sqlite3_exec(conn,"delete from alumnos where cuanta='344'",0,0,0);
+void eliminar()	{//elimina datos
+	cout<<"---eliminar empleados---"<<endl;
+	string empno;
+	cin.ignore(100,'\n');
+	cout<<"\nempno: ";
+	getline(cin,empno);
+	error=sqlite3_open("oracle-sample.db",&conn);
+	string query="delete from emp where empno='"+empno+"'";
+	error=sqlite3_exec(conn,query.c_str(),0,0,0);
+	if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
 	sqlite3_close(conn);	
+}
+/*
+void jefeEmpleados(string empno){
+	string jefe=;
+	string query=;
+	error=sqlite3_open("oracle-sample.db",&conn);
+	query="select mgr from emp where empno='"+empno+"'";
+	error=sqlite3_prepare_v2(conn,query.c_str(),1000,&res,&tail);
+	if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+		return;
+	}
+	jefe=(char*)sqlite3_column_text(res,0);
+	cout<<"jefe: "<<jefe;
+	sqlite3_close(conn);        
+}
+*/
+void jefeEmpleados(string empno){
+	string query;
+	string jefe;
+	error=sqlite3_open("oracle-sample.db",&conn);
+	query="select mgr from emp where empno='"+empno+"'";
+	error=sqlite3_prepare_v2(conn,query.c_str(),query.length()+1,&res,&tail);
+    if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	if(sqlite3_step(res) == SQLITE_ROW){
+		cout<<"jefe: "<<sqlite3_column_text(res,0)<<endl;
+		jefe=(char*)sqlite3_column_text(res,0);
+		cout<<"\n--empleados de ese jefe-- "<<endl;
+		query="select ename from emp where mgr='"+jefe+"'";
+		error=sqlite3_prepare_v2(conn,query.c_str(),query.length()+1,&res,&tail);
+		if(error!=SQLITE_OK){
+			cout<<"\nsomething wrong /check query\n";
+		}
+		while(sqlite3_step(res) == SQLITE_ROW){
+			cout<<sqlite3_column_text(res,0)<<endl;
+		}
+		
+	}else{
+		cout<<"no hay empleado";
+	}	
+	sqlite3_close(conn);
+}
+void depto(string ndepa){
+	string query;
+	error=sqlite3_open("oracle-sample.db",&conn);
+	query="select dname from dept where deptno='"+ndepa+"'";
+	error=sqlite3_prepare_v2(conn,query.c_str(),query.length()+1,&res,&tail);
+    if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	cout<<"\n---nombre departamentos--"<<endl;
+	while(sqlite3_step(res) == SQLITE_ROW){
+		cout<<sqlite3_column_text(res,0)<<endl;
+	}
+	cout<<"\n--empleados de ese departamento---"<<endl;
+	query="select ename from emp where deptno='"+ndepa+"'";
+	error=sqlite3_prepare_v2(conn,query.c_str(),query.length()+1,&res,&tail);
+    if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	while(sqlite3_step(res) == SQLITE_ROW){
+		cout<<sqlite3_column_text(res,0)<<endl;
+	}
+	cout<<"\n--salario total de todos los empleados---"<<endl;
+	query="select sal from emp where deptno='"+ndepa+"'";
+	error=sqlite3_prepare_v2(conn,query.c_str(),query.length()+1,&res,&tail);
+    if(error!=SQLITE_OK){
+		cout<<"\nsomething wrong /check query\n";
+	}
+	int sum=0;
+	while(sqlite3_step(res) == SQLITE_ROW){
+		sum+=atoi((char*)sqlite3_column_text(res,0));
+	}
+	cout<<"\nsum: "<<sum;
+	sqlite3_close(conn);
+	
+	
 }
